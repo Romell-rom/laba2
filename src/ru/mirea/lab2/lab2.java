667830408b810c311243458afbc1d3c79ce5f41f
Romich <70742227+Romell-rom@users.noreply.github.com>
2020-09-11 08:28:23 +0300

    Update lab2.java

--- src/ru/mirea/lab2/lab2.java
+++ src/ru/mirea/lab2/lab2.java
@@ -2,17 +2,38 @@ package ru.mirea.lab2;
 import java.util.Scanner;
 public class lab2 {
 public static void main(String[] args){
-    //Task 1 (Shape)
-   // Shape triangle = new Shape();
-  //  System.out.print(triangle.k);
-   // triangle.say("3");
-   //Task 2 (Ball)
+    /*Task 1 (Shape)
+    Shape triangle = new Shape();
+    System.out.print(triangle.k);
+    triangle.say("3");*/
+   /*Task 2 (Ball)
     Ball jabulani = new Ball();
     System.out.println(jabulani.form);
     System.out.println(jabulani.made);
-    System.out.println(jabulani.pressure);
-    System.out.println(jabulani.weight);
+   System.out.println(jabulani.pressure);
+   System.out.println(jabulani.weight); */
+    // taks 3(book)
+    Book king = new Book();
+      System.out.println(king.form);
+     System.out.println(king.made);
+     System.out.println(king.weight);
+    System.out.println(king.toString());
+    /*Taks 4
+    Dog dog = new Dog(7,"Rich");
+    Scanner scanAge = new Scanner(System.in);
+    Scanner scanName = new Scanner(System.in);
+    int age;
+    String name;
+    System.out.print("Введите возраст собаки - ");
+    age = scanAge.nextInt();
+    dog.setAge(age);
+    System.out.print("Введите кличку собаки - ");
+    name = scanName.nextLine();
+    dog.setName(name);
+    System.out.println("Возраст собаки - " + dog.getAge());
+    System.out.println("Кличка собаки - " + dog.getName());
+    System.out.println("Возраст собаки в человеческой форме - " + dog.res(dog.getAge()));
+    System.out.println("Вывод с помощью toString - " + dog.toString());*/
 }
 
 }
-
